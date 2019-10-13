8263677251af4147f0d6336d1ae9e438868c415f
tpalecek1 <36023933+tpalecek1@users.noreply.github.com>
2019-10-13 10:32:22 -0700

    Refactored cardEffects

--- projects/palecekt/dominion/dominion_helpers.h
+++ projects/palecekt/dominion/dominion_helpers.h
@@ -12,4 +12,15 @@ int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
 
+//Refactored card effects
+int baronEffect(int choice1, struct gameState *state, int currentPlayer, int handPos);
+int minionEffect(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int ambassadorEffect(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int tributeEffect(struct gameState *state, int currentPlayer, int nextPlayer, int tributeRevealedCards[], int handPos);
+int mineEffect(int choice1, int choice2, struct gameState *state, int currentPlayer, int handPos);
+
+//Helper functions for the refactored cardeffects
+int discardHand(struct gameState *state, int player);
+int drawX(struct gameState *state, int numDraws, int player);
+
 #endif
